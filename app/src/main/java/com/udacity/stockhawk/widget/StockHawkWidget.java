67c57d0f07e66042219f7a28d8207a425ac1298d
Borja Quevedo <borjota@gmail.com>
2017-04-21 11:14:57 +0200

    Remove empty overrides

--- app/src/main/java/com/udacity/stockhawk/widget/StockHawkWidget.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockHawkWidget.java
@@ -22,19 +22,9 @@ public class StockHawkWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        //Update all possible widgets
+        for (int id : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, id);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
